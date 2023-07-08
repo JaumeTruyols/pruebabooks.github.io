91fa235a2d5f90d244727ba88a94df0ea1022940
jaume <jaumetruyols@paucasesnovescifp.cat>
2023-07-08 21:34:26 +0200

    actu3

---
+++ src/data/books.js
@@ -0,0 +1,78 @@
+export const books = [
+    {
+        id: 'clean-code',
+        title: 'Clean Code: Manual de desarrollo ágil de software',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+        opinion: 'Un clásico entre los clásicos del mundo de la programación. Aunque en pleno 2023 yo tengo una opinión un poco polémica. No me parece un libro que recomendaría a todo el mundo, especialmente a aquellas personas que están aprendiendo. Además, tiene un tono muy dogmático y existe gente que se lo toma, peligrosamente, como la biblia del desarrollo. Pero si eres capaz de leerlo tomando perspectiva, hay cosas interesantes que todavía están vigentes. Ojo que todos los ejemplos de código están basados en Java y mucha programación orientada a objetos.',
+        author: 'Robert C. Martin',
+    },
+    {
+        id: 'aprendiendo-git',
+        title: 'Aprendiendo Git',
+        img: "https://d2sofvawe08yqg.cloudfront.net/aprendiendo-git/s_hero2x?1628527844",
+        opinion: 'Un libro muy interesante para aprender Git. Mucho más allá de los comandos. Habla de buenas prácticas, estrategias de trabajo en equipo y flujos para mejorar la entrega de software. Además, está escrito por un autor español, lo que siempre es de agradecer.',
+        author: 'Miguel Angel Durán'
+    },
+    {
+        id: 'the-clean-coder',
+        title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+        img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+        opinion: '"The Clean Coder: Un Código de Conducta para Programadores Profesionales" es un libro excepcional que revoluciona la forma en que los programadores abordan su trabajo. Con sus ideas prácticas e inspiradoras, establece un nuevo estándar de profesionalismo en la industria del desarrollo de software. Su enfoque en la responsabilidad personal, el aprendizaje continuo y la comunicación efectiva, promueve la entrega de código limpio, la mejora constante y la colaboración en equipo. En resumen, este libro es una herramienta indispensable que empodera a los programadores para alcanzar la excelencia y contribuir al éxito de sus organizaciones.',
+        author: 'Robert C. Martin',
+    },
+    {
+        id: 'javascript-the-good-parts',
+        title: 'JavaScript: The Good Parts',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+        opinion: '"JavaScript: The Good Parts" es un libro imprescindible que destaca las mejores prácticas y características esenciales de JavaScript. A través de una visión concisa y perspicaz, el autor nos guía por el mundo de este lenguaje, revelando su potencial y resaltando sus aspectos más poderosos y efectivos. Con un enfoque en la simplicidad y la elegancia, el libro ofrece una guía práctica para aprovechar al máximo las partes más sólidas y confiables de JavaScript, permitiéndonos escribir un código claro y eficiente. En resumen, esta obra es una valiosa herramienta para cualquier desarrollador que desee dominar el arte de programar con JavaScript.',
+        author: 'Douglas Crockford',
+    },
+    {
+        id: 'eloquent-javascript',
+        title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
+        opinion: '"Eloquent JavaScript" es un libro extraordinario que lleva a los lectores en un viaje apasionante a través del mundo de la programación en JavaScript. Con un enfoque amigable y accesible, el autor nos introduce en los conceptos fundamentales del lenguaje y nos guía hábilmente a través de ejercicios prácticos y desafiantes. Con una combinación equilibrada de teoría y práctica, el libro fomenta el pensamiento lógico y creativo, y nos enseña a desarrollar soluciones elegantes y eficientes. En resumen, "Eloquent JavaScript" es una guía imprescindible para aquellos que desean dominar este poderoso lenguaje de programación y explorar todo su potencial.',
+        author: 'Marijn Haverbeke',
+    },
+    {
+        id: 'design-patterns',
+        title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
+        img: 'https://m.media-amazon.com/images/I/51JYkEpbhzL._SY466_.jpg',
+        opinion: '"Design Patterns: Elements of Reusable Object-Oriented Software" es un clásico en el mundo del desarrollo de software. Escrito por el grupo conocido como "Gang of Four" (Erich Gamma, Richard Helm, Ralph Johnson y John Vlissides), este libro presenta una colección de patrones de diseño que ofrecen soluciones probadas y reutilizables a problemas comunes en el diseño de software orientado a objetos. Con explicaciones claras y ejemplos prácticos, el libro ayuda a los desarrolladores a mejorar la estructura y flexibilidad de sus sistemas, promoviendo la modularidad, la reutilización y la mantenibilidad del código.',
+        author: 'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides',
+    },
+    {
+        id: 'the-pragmatic-programmer',
+        title: 'The Pragmatic Programmer: Your Journey to Mastery',
+        img: 'https://m.media-amazon.com/images/I/71f743sOPoL._SY466_.jpg',
+        opinion: '"The Pragmatic Programmer: Your Journey to Mastery" es un libro esencial para todo programador que aspire a alcanzar la maestría en su oficio. Escrito por Andrew Hunt y David Thomas, este libro ofrece consejos prácticos y perspectivas únicas sobre diversos aspectos del desarrollo de software, incluyendo el diseño, la implementación, la depuración, el rendimiento y más. A través de anécdotas, ejemplos y principios fundamentales, los autores nos guían en un viaje de crecimiento profesional, fomentando habilidades técnicas y actitudes pragmáticas que nos ayudarán a enfrentar los desafíos del desarrollo de software de manera efectiva.',
+        author: 'Andrew Hunt, David Thomas',
+    },
+    {
+        id: 'refactoring',
+        title: 'Refactoring: Improving the Design of Existing Code',
+        img: 'https://m.media-amazon.com/images/I/81ezCJTXFKL._SY466_.jpg',
+        opinion: '"Refactoring: Improving the Design of Existing Code" es un libro imprescindible para aquellos desarrolladores que deseen mejorar la calidad y mantenibilidad de su código existente. Escrito por Martin Fowler, este libro presenta técnicas prácticas y patrones de refactorización que nos permiten reestructurar el código de manera incremental, sin cambiar su funcionalidad externa. A través de ejemplos detallados y explicaciones claras, el autor nos muestra cómo identificar y aplicar refactorizaciones efectivas, mejorando así la legibilidad, flexibilidad y rendimiento del software.',
+        author: 'Martin Fowler',
+    },
+
+    {
+        id: 'the-mythical-man-month',
+        title: 'The Mythical Man-Month: Essays on Software Engineering',
+        img: 'https://m.media-amazon.com/images/I/51Al66uQmcL._SY445_SX342_.jpg',
+        opinion: '"The Mythical Man-Month: Essays on Software Engineering" es un libro clásico que aborda los desafíos y problemas comunes en el desarrollo de software. Escrito por Frederick P. Brooks Jr., este libro ofrece una colección de ensayos que exploran aspectos clave de la ingeniería de software, como la gestión de proyectos, la productividad del equipo, la planificación y más. A través de experiencias reales y reflexiones profundas, el autor nos brinda valiosas lecciones sobre cómo abordar de manera efectiva el desarrollo de software a gran escala.',
+        author: 'Frederick P. Brooks Jr.',
+    },
+
+    {
+        id: 'domain-driven-design',
+        title: 'Domain-Driven Design: Tackling Complexity in the Heart of Software',
+        img: 'https://m.media-amazon.com/images/I/61aFldsgAmL._SY466_.jpg',
+        opinion: '"Domain-Driven Design: Tackling Complexity in the Heart of Software" es un libro fundamental para aquellos que desean desarrollar software de calidad y con un enfoque en el dominio del problema. Escrito por Eric Evans, este libro presenta principios y patrones de diseño que ayudan a los equipos de desarrollo a comprender y modelar mejor el dominio en el que trabajan. A través de ejemplos prácticos y estrategias claras, el autor nos muestra cómo construir sistemas software de alta calidad, centrados en el negocio y capaces de adaptarse a los cambios en los requisitos.',
+        author: 'Eric Evans',
+    },
+    {
+        id: '404',
+        img: 'https://media.tenor.com/Aa8mokQYPQ8AAAAd/adult-swim-this-is-fine.gif'
+    }
+]
