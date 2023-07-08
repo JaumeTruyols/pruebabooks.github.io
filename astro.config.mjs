d8e5b7e8a0841decc1ff30dac4b259f7d5e12e90
jaume <jaumetruyols@paucasesnovescifp.cat>
2023-07-08 22:29:44 +0200

    actu12

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "dist", // Asegúrate de tener esta opción configurada
   adapter: node({
     mode: "standalone"
   }),
   integrations: [tailwind()],
-  site: 'https://jaumetruyols.github.io/pruebabooks.github.io/'
+site: 'https://jaumetruyols.github.io/pruebabooks.github.io/'
 });
